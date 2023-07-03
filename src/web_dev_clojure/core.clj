6972b97e6097583f6c112a9dc576d94a243cdf50
Elton Peixoto <elton.peixoto@nubank.com.br>
2023-07-03 17:47:18 -0300

    refatoring hello word

--- src/web_dev_clojure/core.clj
+++ src/web_dev_clojure/core.clj
@@ -1,6 +1,12 @@
 (ns web-dev-clojure.core
   (:require [ring.adapter.jetty :as jetty]))
 
+
+(defn greet [req]
+  {:status  200
+   :body    "Hello, World!"
+   :headers {}})
+
 (defn -main [port]
-  (jetty/run-jetty (fn [req] {:status 200 :body "Hello, World!" :headers {}})
+  (jetty/run-jetty greet
                    {:port (Integer. port)}))
